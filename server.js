e53ec3343f537ff2188b390a67a6b522c6ffef83
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-05 13:39:08 -0700

    adding psuedo code from instructor demo

---
+++ server.js
@@ -0,0 +1,20 @@
+//require Express
+
+//require the db.json file and store it in 'notes'
+
+const note= "db.json";
+
+//use express to initialize the 'app' server
+
+//GET /notes should return the notes.html file.
+
+//Get * should return the index.html file
+
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+    // 
+    // res.json()
+
+//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+//Use the 'app' to listen to a specific 'PORT'
