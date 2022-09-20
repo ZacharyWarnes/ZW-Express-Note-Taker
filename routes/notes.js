5e5b13833f846ed7c1acebf82af08b087082eaf7
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-19 20:37:24 -0700

    moving routes to notes.js in routes folder. changing res.json for readAndAppend new note

---
+++ routes/notes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+const notesRouter = express.Router();
+const db = require('../db/db.json');
+
+
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('api/notes', (req,res) => {
+    console.info(`${req.method} request received for notes`)
+    readFromFile('../db/db.json').then((data)=> res.json(JSON.parse(data)));
+  });
+  
+  //POST Route for submitting note
+  app.post('api/notes', (req,res) => {
+    //POST note request rec
+    console.info(`${req.method} request received to post note`);
+  
+    //Destructing assignment for the items in req.body
+    const { text, title } = req.body;
+  
+    //If all the required properties are present
+    if (text && title) {
+      //Variable for the object to save
+      const newNote = {
+        text,
+        title,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, '../db/db.json');
+        res.json(`note added successfully`);
+    } else {
+        res.error('Error adding note')
+    } 
+});
+  
+
+module.exports = notesRouter;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const db = require('./db/db.json');
+
 const uuid = require('./helpers/uuid');
 
 const PORT = process.env.port || 3001;
@@ -16,50 +16,20 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 //GET Route for homepage
-app.get('/', (req, res) =>
+app.get('/', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 //GET Route for notes page
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get('api/notes', (req,res) => {
-  console.info(`${req.method} request received for notes`)
-  readFromFile('db').then((data)=> res.json(JSON.parse(data)));
-});
+//Wildcard route default to index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html')) 
+);
 
-//POST Route for submitting note
-app.post('api/notes', (req,res) => {
-  //POST note request rec
-  console.info(`${req.method} request received to post note`);
-
-  //Destructing assignment for the items in req.body
-  const { text, title } = req.body;
-
-  //If all the required properties are present
-  if (text && title) {
-    //Variable for the object to save
-    const newNote = {
-      text,
-      title,
-      note_id: uuid(),
-    };
-
-    readAndAppend(newNote, 'db');
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    res.json(response);
-  } else {
-    res.json('Error posting note')
-  }
-});
 
 //Using app to listen to designated PORT
 app.listen(PORT, () =>
