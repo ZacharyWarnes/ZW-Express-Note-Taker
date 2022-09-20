8ae6c230060f50d2f12d719329dc66c9aba58479
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-19 20:10:16 -0700

    adding index.js for routes index. adding notes.js to organize GET/POST routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+//Import modular router for notes
+const notesRouter = require('./notes');
+
 // Initializing app 
 const app = express();
+
+//Initializing notes route
+app.use('/notes', notesRouter);
 
 module.exports = app;
