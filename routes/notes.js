6325bb08a38df59c58e56028d2ce918a1cc1bd53
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-22 17:40:29 -0700

    adding notesRouter to notes.js for GET and POST routes. changing file path for db

--- routes/notes.js
+++ routes/notes.js
@@ -2,17 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const uuid = require('../helpers/uuid');
 const notesRouter = express.Router();
-const db = require('../db/db.json');
+const db = require('./db/db.json');
 
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get('api/notes', (req,res) => {
+notesRouter.get('api/notes', (req,res) => {
     console.info(`${req.method} request received for notes`)
-    readFromFile('../db/db.json').then((data)=> res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)));
   });
   
   //POST Route for submitting note
-  app.post('api/notes', (req,res) => {
+  notesRouter.post('api/notes', (req,res) => {
     //POST note request rec
     console.info(`${req.method} request received to post note`);
   
@@ -28,7 +28,7 @@ app.get('api/notes', (req,res) => {
         note_id: uuid(),
       };
   
-      readAndAppend(newNote, '../db/db.json');
+      readAndAppend(newNote, './db/db.json');
         res.json(`note added successfully`);
     } else {
         res.error('Error adding note')
