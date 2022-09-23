101509d000931c4cf4ead56006261e26e4fbbd9e
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-22 17:32:01 -0700

    adding api route to server page and linking index.js in routes folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const api = require('./routes/index')
 
 const PORT = process.env.port || 3001;
 
@@ -11,6 +12,8 @@ const app = express();
 //Middleware for parsing JSON and url-encoding form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/api', api)
+
 app.use(express.static('public'));
 
 //GET Route for homepage
